ddbbc38758964477bf5c5f2f6a8a5622169aedfc
Eevee (Evelyn Woods) <eevee.git@veekun.com>
2019-09-05 08:06:26 -0600

    Fix blank loading screen when waiting on a Google font

--- gleam-player.js
+++ gleam-player.js
@@ -112,7 +112,13 @@ class GoogleFontLoader {
 
         this.loaded_fonts[family] = true;
 
-        let params = new URLSearchParams({family: family});
+        let params = new URLSearchParams({
+            family: family,
+            // This adds font-display: swap; to each @font-face block, which
+            // asks the browser to use a fallback font while the web font is
+            // downloading -- this avoids invisible text on the loading screen
+            display: 'swap',
+        });
         document.head.append(mk('link', {
             href: `https://fonts.googleapis.com/css?${params}`,
             rel: 'stylesheet',
